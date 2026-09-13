f260b832f5c40f3e1bdd3c2f5ed03cd645c1acd4
Hyilix <sebi.ursescu@gmail.com>
2026-09-14 00:53:15 +0300

    Added basic window implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use winit::application::ApplicationHandler;
-use winit::event_loop::{EventLoop};
-use winit::window::Window;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::event::WindowEvent;
+use winit::window::{Window, WindowId, WindowAttributes};
 
 #[derive(Default)]
 struct App {
@@ -8,7 +9,33 @@ struct App {
 }
 
 impl ApplicationHandler for App {
+    fn can_create_surfaces(
+        &mut self,
+        event_loop: &dyn ActiveEventLoop,
+    ) {
+        self.window = Some(event_loop.create_window(WindowAttributes::default()).unwrap());
+    }
 
+    fn window_event(
+        &mut self,
+        event_loop: &dyn ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("Window Closed via Request");
+                event_loop.exit();
+            },
+            WindowEvent::Focused(state) => {
+                println!("Window focus is now {state}");
+            },
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            },
+            _ => (),
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
